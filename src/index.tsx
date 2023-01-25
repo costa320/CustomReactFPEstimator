6ddf77baa93c5e17eddd87410c556f78d498c7f4
constantin <33323432+costa320@users.noreply.github.com>
2023-01-25 19:21:37 +0100

    +

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
+/** @format */
+
 import ReactDOM from 'react-dom/client';
 
-import App from "./App";
+import App from './App';
 
-const elem :any= document.getElementById('root');
-const root:any = ReactDOM.createRoot(elem?elem:<span></span>);
+const elem: any = document.getElementById('root');
+const root: any = ReactDOM.createRoot(elem ? elem : <span></span>);
 
-root.render(
-
-    <App />
- 
-);
+root.render(<App />);
